bbba05370c9c386f85c4cc54c5bb81cb1ea46140
John Sitler <johnsitler@Johns-MacBook-Pro.local>
2018-08-27 21:59:47 +0400

    Lesson 01 complete

---
+++ coder-swag-v2/coder-swag-v2/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag-v2
+//
+//  Created by John Sitler on 8/27/18.
+//  Copyright © 2018 JohnSitler. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
